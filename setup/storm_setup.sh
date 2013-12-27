b5d5a7ba1320da411bd60d12941a56559b6efa7e
qiujw <qiujw@hadoop2.(none)>
2013-12-27 15:35:32 +0800

    fix bug

--- setup/storm_setup.sh
+++ setup/storm_setup.sh
@@ -12,12 +12,10 @@ if [ ! -d "$HOME/storm" ]
 then
     echo "storm 不存在 部署storm"
     #wget https://github.com/downloads/nathanmarz/storm/storm-0.8.1.zip
-    wget https://dl.dropboxusercontent.com/s/dj86w8ojecgsam7/storm-0.9.0.1.zip
-    NAME=${storm-0.9.0.1}
-    unzip $NAME.zip
-    mv $NAME ~/
-    cd ~/
-    ln -s $NAME storm
+    #wget https://dl.dropboxusercontent.com/s/dj86w8ojecgsam7/storm-0.9.0.1.zip
+    NAME=storm-0.9.0.1
+    unzip ${NAME}.zip
+    ln -s ${NAME} storm
     cp $HELP/bin/*  $HOME/storm/bin/
     cp $HELP/conf/* $HOME/storm/conf/
     cp $HELP/storm_profile $HOME/.storm_profile
