b93fc95c9e74051f1132d3622907955f30885ec8
qus-jiawei <qus-jiawei@gmail.com>
2014-01-22 12:13:48 +0800

    ci all

--- help/bin/stop-supervisor.sh
+++ help/bin/stop-supervisor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "stoping supervisors"
 
-sh ${STORM_BIN}/supervisors.sh " ps aux | grep 'backtype.storm.daemon.supervisor'|grep -v grep | awk '{print $1}' | xargs kill"
+sh ${STORM_BIN}/supervisors.sh " ps aux | grep 'backtype.storm.daemon.supervisor'|grep -v grep | awk '{print $2}' | xargs kill"
 
