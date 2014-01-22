4c0f3f34ea6c55c8eba3819a6ca323f346c484c8
qus-jiawei <qus-jiawei@gmail.com>
2014-01-22 12:16:33 +0800

    fix bug

--- help/bin/stop-drpc.sh
+++ help/bin/stop-drpc.sh
@@ -2,5 +2,5 @@
 echo "stoping drpc"
 . ${STORM_CONF}/storm-env.sh
 
-sh ${STORM_BIN}/drpcs.sh ". ~/.bash_profile ;ps aux |grep drpc|awk '{print \$1}'|xargs kill "
+sh ${STORM_BIN}/drpcs.sh ". ~/.bash_profile ;ps aux |grep drpc|awk '{print \$2}'|xargs kill "
 
--- help/bin/stop-worker.sh
+++ help/bin/stop-worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sh ${STORM_BIN}/supervisors.sh " ps aux | grep 'backtype.storm.daemon.worker'|grep -v grep | awk '{print $1}' | xargs kill"
+sh ${STORM_BIN}/supervisors.sh " ps aux | grep 'backtype.storm.daemon.worker'|grep -v grep | awk '{print \$2}' | xargs kill"
 
