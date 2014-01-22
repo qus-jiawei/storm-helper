2dddda19b7b9d0af92825c7fad7c69a9a0cf89f3
qus-jiawei <qus-jiawei@gmail.com>
2014-01-22 12:18:36 +0800

    fix bug

--- help/bin/stop-ui.sh
+++ help/bin/stop-ui.sh
@@ -2,5 +2,5 @@
 echo "stoping ui"
 . ${STORM_CONF}/storm-env.sh
 
-ps aux |grep 'backtype.storm.ui.core'|grep -v grep |awk '{print \$2}' |xargs kill
+ps aux |grep 'backtype.storm.ui.core'|grep -v grep |awk '{print $2}' |xargs kill
 
